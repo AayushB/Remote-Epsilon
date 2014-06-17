cf86c65a482ef8adddbc72e2f7a97622926e87e7
WilsonHoang <wilson.hoang11@yahoo.com>
2014-06-17 00:17:50 -0700

    Added collisionController Files

---
+++ libraries/collisionController/collisionController.cpp
@@ -0,0 +1,19 @@
+#include "collisionController.h"
+
+collisionController::collisionController()
+{
+
+}
+
+
+
+bool collisionController::frontDetect()//detects distance from object in front
+{
+
+
+}
+void collisionController::detectionResponse()
+{
+
+
+}

cb8d93323e8759c97eb2996d5aa7e2ecb35986c4
AayushB <Aayush@purpamine.bh>
2014-06-19 18:12:37 -0700

    Update CollisionController with Servo
    Servo Movement to return the safe distance.

--- libraries/collisionController/collisionController.cpp
+++ libraries/collisionController/collisionController.cpp
@@ -1,17 +1,60 @@
-#include "collisionController.h"
-
-collisionController::collisionController(int echoPin, int triggerPin)
+/**
+ *CollisionController
+ *Copyright 2014
+ *Written by Aayush Bhandari, Wilson Hoang
+ *6/19/2014
+ **/
+#include "CollisionController.h"
+CollisionController::CollisionController(int echoPin, int triggerPin,int servoPin)
 {
     this->echoPin=echoPin;
     this->triggerPin=triggerPin;
+    this->servoPin=servoPin;
     int MAX_DISTANCE=500;
     
-    sonar= new NewPing(echoPin,triggerPin,MAX_DISTANCE);
+    sonar= new NewPing(echoPin,triggerPin,MAX_DISTANCE);//creating NewPing object
+    //sonar
+    
+    angle=90;
+    myServo.attach(servoPin);
+    myServo.write(angle);
+    //delay without using delay();
+    prevMillis=0;
+    interval=60;
+    
 }
-double recieveDistance(){
-    int uS=sonar->ping();
-    distance=uS/US_ROUNDTRIP_CM;
-    Serial.print(distance);
-    Serial.println("cm");
+double CollisionController::recieveDistance()
+{
+    int uS=sonar->ping();// gets the distance and set it to uS to convert to cm later
+    distance=uS/US_ROUNDTRIP_CM;//converting the uS to cm and setting it to distance
     return distance;
+}
+
+
+int CollisionController::recieveAngle()
+{
+    int myAngle;
+    double maxDistance=0;
+    
+    myServo.write(0);
+    
+    for(int currentAngle=0; currentAngle < 200; )
+    {
+        unsigned long currMillis=millis();//sets the current time to the time since
+
+        if(currMillis-prevMillis  > interval)// 60 ms delay
+        {
+            prevMillis=currMillis;// set the previous time to current time
+            myServo.write(currentAngle);//assigning currentAngle to servo
+            double distance= sonar->ping();//determining distance
+            if(maxDistance < distance)
+            {
+                myAngle=currentAngle;//setting myAngle if the distance at said angle is has the largest distance
+                maxDistance=distance;//setting the distance at angle
+            }
+            currentAngle+=10;
+        }
+    }
+    myServo.write(angle);
+    return myAngle;
 }
--- libraries/collisionController/collisionController.h
+++ libraries/collisionController/collisionController.h
@@ -1,18 +1,42 @@
+/**
+ *CollisionController
+ *Copyright 2014
+ *Written by Aayush Bhandari, Wilson Hoang
+ *6/19/2014
+ **/
 #ifndef COLLISIONCONTROLLER_H
 #define COLLISIONCONTROLLER_H
 #include "Arduino.h"
 #include <NewPing.h>
+#include <Servo.h>
 
-class collisionController
+class CollisionController
 {
 public:
-    collisionController(int echoPin,triggerPin);
+    CollisionController(int echoPin,int triggerPin, int servoPin);
+    //echoPin controls the input of the HC-SR04 which emitts the ping
+    //triggerPin controls the input of the HC-SR04 which recieves the echo which is used to compute the distance
     double recieveDistance();
+    //takes in the distance from sonar object which is converted to cm and then printed.
     
-protected:
+    int recieveAngle();
+    //recieves the angle at which the HC-SR04 takes in the largest distance.
+    
 private:
+    //This is part of HC-SR04
     double distance;
     NewPing* sonar;
+    int echoPin;
+    int triggerPin;
+    //this is part of the servo moto
+    int angle;
+    Servo myServo;
+    int servoPin;
+    //this is part the time delay without using delay();
+    int prevMillis;
+    int interval;
+
+    
 };
 
 #endif
